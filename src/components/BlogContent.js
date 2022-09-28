9ddb54e96b61bfce05d9143a5016935b9ae4897b
Realmunene <josep.mwangi@student.moringaschool.com>
2022-09-28 23:53:29 +0300

    completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  if(!props.isPublished){
+    return null;
+  }else{
+    return (
+    <h1>{props.articleText}</h1>;
+    <p>{props.minutesToRead} minutes to read</p>
+    )
+
 }
 
 export default BlogContent;
